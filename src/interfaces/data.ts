6f81fcb2afc43f033f8bf5a70392729a39aa0bd5
Juan Carlos Carrasquilla <juan.carrasquilla@edu.gov.co>
2024-10-07 17:24:38 -0500

    add docs

---
+++ src/interfaces/data.ts
@@ -0,0 +1,20 @@
+export interface IContratos {
+  id: number;
+  ci: string;
+  proyecto: string;
+  comuna: number;
+  tipologia: string;
+  ie: string;
+  contrato: string;
+  vigencia: number;
+  vlrContrato: number;
+  estado: string;
+  inicio: number;
+  final: number;
+  reciboSEM: number;
+  "prog%": number;
+  "ejec%": number;
+  presIncumi?: string;
+  entregaSEM?: string;
+  linkBuild: string;
+}
